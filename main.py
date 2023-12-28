524f86b8ae8e7d79de100151b200d9575f8967e3
Damian Śnieda <damiansnieda97@gmail.com>
2023-12-28 21:52:11 +0100

    Main function

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import pyautogui, time
+from src.CycleRecorder import RecordActions
+from src.Repeater import RepeatAction
 
-distance = 200
-time.sleep(5)
-while distance > 0:
-        pyautogui.drag(distance, 0, duration=0.5)   # move right
-        distance -= 5
-        pyautogui.drag(0, distance, duration=0.5)   # move down
-        pyautogui.drag(-distance, 0, duration=0.5)  # move left
-        distance -= 5
-        pyautogui.drag(0, -distance, duration=0.5)  # move up
+if __name__ == '__main__':
+    sequence = RecordActions()
+    RepeatAction(sequence)
