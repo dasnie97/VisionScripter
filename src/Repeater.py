c10e56b0afbbb6291fc5b2c1caf642bd1276ab64
Damian Śnieda <damiansnieda97@gmail.com>
2023-12-29 21:06:14 +0100

    added clicks and typing

--- src/Repeater.py
+++ src/Repeater.py
@@ -3,5 +3,8 @@ import pyautogui
 def RepeatAction(sequence):
     for action in sequence:
         if isinstance(action, tuple):
-            pyautogui.moveTo(*action, 0.5)
-        
+            pyautogui.moveTo(*action, 0.3)
+            pyautogui.leftClick()
+        if isinstance(action, str):
+            pyautogui.write(action)
+    print("Koniec!")
