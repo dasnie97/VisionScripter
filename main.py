3aff0a94268229bafefe4b62e7ffd6d41cb27e4a
Damian Śnieda <damiansnieda97@gmail.com>
2023-12-29 21:05:15 +0100

    new function for input conversion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.CycleRecorder import RecordActions
 from src.Repeater import RepeatAction
+from src.InputConverter import Convert, converted
 
 if __name__ == '__main__':
+    Convert(r'C:\Users\Damian\source\repos\VisionScripter\test.txt')
     sequence = RecordActions()
     RepeatAction(sequence)
