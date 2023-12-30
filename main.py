47b6c594edd1d20f84a2a44339ed7319e37b0d67
Damian Śnieda <damiansnieda97@gmail.com>
2023-12-30 13:13:08 +0100

    creating sequence using user input

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.CycleRecorder import RecordActions
 from src.Repeater import RepeatAction
-from src.InputConverter import Convert, converted
+from src.InputConverter import *
+from src.CreateSequence import *
 
 if __name__ == '__main__':
     Convert(r'C:\Users\Damian\source\repos\VisionScripter\test.txt')
     sequence = RecordActions()
+    sequence = CreateSequence(sequence, converted)
     RepeatAction(sequence)
