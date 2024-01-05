d424fad8ea0cc5bdc8af686f1afde59067d26422
Damian Śnieda <damiansnieda97@gmail.com>
2024-01-05 21:12:07 +0100

    run MainWindow on startup

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from src.CycleRecorder import RecordActions
-from src.Repeater import RepeatAction
-from Helpers import *
-from src.CreateSequence import *
+from src.MainWindow import MainWindow
 
 if __name__ == '__main__':
-    Convert(r'C:\Users\Damian\source\repos\VisionScripter\test.txt')
-    sequence = RecordActions()
-    sequence = CreateSequence(sequence, converted)
-    RepeatAction(sequence)
+    application = MainWindow()
