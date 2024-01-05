facfd3795531c0a6bcfb1d098e14f1cbe873b989
Damian Śnieda <damiansnieda97@gmail.com>
2024-01-05 18:13:02 +0100

    Created one class SequenceRecorder from ConfigManager and CycleRecorder.

--- src/ConfigManager.py
+++
@@ -1,8 +0,0 @@
-sequence = []
-
-def append_mouse_position(x, y):
-    sequence.append((x,y))
-
-def append_text_input(text):
-    if text != "":
-        sequence.append(text)
--- src/Helpers.py
+++ src/Helpers.py
@@ -1,3 +1,54 @@
+import time
+import keyboard
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
+
+class SequenceRecorder:
+    def __init__(self) -> None:
+        self.word = ""
+        self.sequence = []
+
+    def RecordActions(self):
+        print("Zacznij ruszać kursorem, klikać i wpisywać tekst. Czynności będą nagrywane, a potem odtworzone. Gdy skończysz cykl wciśnij f2.")
+        mouse_listener = MouseListener(on_click=self.on_click)
+        mouse_listener.start()
+        keyboard_listener = KeyboardListener(on_press=self.on_press)
+        keyboard_listener.start()
+
+        try:
+            while True:
+                if keyboard.is_pressed('f2'):
+                    time.sleep(0.3)
+                    break
+
+        except KeyboardInterrupt:
+            pass
+
+        finally:
+            mouse_listener.stop()
+            mouse_listener.join()
+            keyboard_listener.stop()
+            keyboard_listener.join()
+        
+        return self.sequence
+
+    def append_text_input(self, text):
+        if text != "":
+            self.sequence.append(text)
+
+    def on_click(self, x, y, button, pressed):
+        if pressed == True:
+            self.append_text_input(self.word)
+            self.word = ""
+            self.sequence.append((x,y))
+
+    def on_press(self, key):
+        try:
+            self.word += key.char
+        except AttributeError:
+            if key.name == 'space':
+                self.word += ' '
+
 class InputConverter:
     def __init__(self) -> None:
         self.converted = []
@@ -32,3 +83,4 @@ class InputConverter:
 
     def insert_char(self, string, char, index):
         return string[:index] + char + string[index:]
+    
