4adc8efbb6c860d149ee9550c64e9fed057ef3be
Damian Śnieda <damiansnieda97@gmail.com>
2024-01-05 18:49:12 +0100

    New model for storing presence info

---
+++ src/Models/Presence.py
@@ -0,0 +1,6 @@
+class Presence:
+    def __init__(self, name:str, surname:str, entry_time:str, exit_time:str) -> None:
+        self.name = name
+        self.surname = surname
+        self.entry_time = entry_time
+        self.exit_time = exit_time
