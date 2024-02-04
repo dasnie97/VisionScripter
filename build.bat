60deb3707aed1408ab937e00d0795b516dc4bc86
Damian Śnieda <damiansnieda97@gmail.com>
2024-02-04 11:25:47 +0100

    Renamed

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-python -m PyInstaller --onefile --noconsole --add-data "assets/A.ico;." --add-data "src/Helpers.py;." --add-data "src/MainWindow.py;." --add-data "src/Models/Presence.py;." main.py
+python -m PyInstaller --onefile --noconsole --add-data "assets/A.ico;." --add-data "src/Helpers.py;." --add-data "src/MainWindow.py;." --add-data "src/Models/Presence.py;." AutoClicker.py
